d0f5f10c774857f63b2e5a56e57c83fa57702878
Nathan Jervis <mirhagk@gmail.com>
2013-11-09 01:08:37 -0500

    Fixed spelling of width

--- asset/src/jQuery/plugins/form/loader.js
+++ asset/src/jQuery/plugins/form/loader.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var loader = $('<span id="loader"><img src="asset/javascript/jQuery/flexiGrid/images/other/spinner.gif"></span>')
-		.css({position: "fixed", top: "50%", left: "50%", widht: "100px", height: "100px", "z-index": "100"})
+		.css({position: "fixed", top: "50%", left: "50%", width: "100px", height: "100px", "z-index": "100"})
 		.appendTo("#pagewidth")
 		.hide();
 	$().ajaxStart(function() {
